ffed61af729a3c8ad676ddf67b9ab46994274283
Susheel <skmjmca@gmail.com>
2023-10-09 18:12:05 +0530

    adding some more insert function in vector

--- push_backAndPop_back.cpp
+++ push_backAndPop_back.cpp
@@ -3,12 +3,17 @@
 using namespace std;
 
 int main(){
-    vector<string> vec;
-    vec.push_back("susheel");
-    vec.push_back("kumar");
+    vector<string> vec{};
+    vec.push_back("australia");
+    vec.push_back("u.k");
+    vec.insert(vec.begin()+1,"Newzeland");
+    vec.push_back("NY");
+    
+    
 
     for(auto it:vec){
         cout<<it<<endl;
+        // cout<<*(it.end())<<endl;
     }
     return 0;
 }
